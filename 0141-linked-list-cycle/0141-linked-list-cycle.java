2fcd7eaf7174e2306fc096c49d456eed6a8229e6
Soumen Mondal <117503345+s479mondal@users.noreply.github.com>
2026-02-06 19:22:34 +0530

    Time: 0 ms (100%), Space: 47.2 MB (12.03%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode t=head;
+        if(head==null)
+        {
+            return false;
+        }
+        ListNode fast=head;
+        ListNode normal=head;
+        while(fast!=null && fast.next!=null)
+        {
+            fast=fast.next.next;
+            normal=normal.next;
+            if(fast==normal)
+            {
+                return true;
+            }
+            t=t.next;
+        }
+        return false;
+    }
+}
