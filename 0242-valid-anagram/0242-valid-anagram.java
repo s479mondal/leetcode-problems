8a8e758346130884b6fd103a8846ac561a62dfb6
Soumen Mondal <117503345+s479mondal@users.noreply.github.com>
2026-02-06 22:54:31 +0530

    Time: 4 ms (79.95%), Space: 44.5 MB (74.32%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,9 +1,24 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-       char a[]=s.toCharArray();
-       char b[]=t.toCharArray();
-       Arrays.sort(a);
-       Arrays.sort(b);
-        return Arrays.equals(a,b);
+       if(s.length()!=t.length())
+       {
+        return false;
+       }
+       int l=s.length();
+       int fr[]=new int[26];
+        for(int i=0;i<l;i++)
+        {
+            fr[s.charAt(i)-'a']++;
+            fr[t.charAt(i)-'a']--;
+        }
+        for(int i=0;i<26;i++)
+        {
+            if(fr[i]!=0)
+            {
+                return false;
+            }
+        }
+        return true;
+
     }
 }
