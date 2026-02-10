7c9635a17348d85754340ab9224a496c77f8984b
Soumen Mondal <117503345+s479mondal@users.noreply.github.com>
2026-02-10 19:17:50 +0530

    Time: 1 ms (99.82%), Space: 55.7 MB (45.67%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,19 +1,25 @@
 class Solution {
     public int majorityElement(int[] nums) {
-       int max=nums[0];
-       int cnt=1;
-       for(int i=1;i<nums.length;i++){
-        if(nums[i]==max){
-            cnt++;
-        }
-        else{
-            cnt--;
-            if(cnt==0){
-                cnt=1;
-                max=nums[i];
+        
+        int max=nums[0];
+        int c=1;
+
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[i]==max)
+            {
+                c++;
+            }
+            else
+            {
+                c--;
+                if(c<=0)
+                {
+                    max=nums[i];
+                    c++;
+                }
             }
         }
-       }
-       return max;
+        return max;
     }
 }
