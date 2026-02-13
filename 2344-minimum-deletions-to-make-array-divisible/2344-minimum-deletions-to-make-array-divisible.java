9b05176daa086f14c7a2342ca5fe1a9bacdcc6cb
Soumen Mondal <117503345+s479mondal@users.noreply.github.com>
2026-02-13 18:56:22 +0530

    Time: 19 ms (15.5%), Space: 102.7 MB (30.75%) - LeetHub

--- 2344-minimum-deletions-to-make-array-divisible/2344-minimum-deletions-to-make-array-divisible.java
+++ 2344-minimum-deletions-to-make-array-divisible/2344-minimum-deletions-to-make-array-divisible.java
@@ -1,27 +1,24 @@
 class Solution {
     public int gcd(int a,int b)
     {
-       return b==0 ? a: gcd(b,a%b);
-    }
-    public int p(int nd[])
-    {
-        int a=nd[0];
-        for(int i=1;i<nd.length;i++)
-        {
-            a=gcd(a,nd[i]);
-        }
-        return a;
+        return b==0?a:gcd(b,a%b);
     }
     public int minOperations(int[] nums, int[] numsDivide) {
+        int g=numsDivide[0];
+        for(int i=1;i<numsDivide.length;i++)
+        {
+            g=gcd(g,numsDivide[i]);
+        }
+
         Arrays.sort(nums);
-        int a=p(numsDivide);
+            System.out.println(g);
         for(int i=0;i<nums.length;i++)
         {
-            if(a%nums[i]==0)
-            {
-                return i;
-            }
+          if(g%nums[i]==0)
+          {
+            return i;
+          }      
         }
-        return -1;
+     return -1;     
     }
 }
