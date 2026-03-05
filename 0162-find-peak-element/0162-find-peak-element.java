b544cd81e341b71a1bec1e5382ca138deabf4bf9
Soumen Mondal <117503345+s479mondal@users.noreply.github.com>
2026-03-05 22:46:51 +0530

    Time: 0 ms (100%), Space: 44.3 MB (35.19%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,31 +1,27 @@
 class Solution {
     public int findPeakElement(int[] nums) {
+        int left=0;
+        int right=nums.length-1;
         if(nums.length==0)
         {
-            return -1;
+            return 0;
         }
         if(nums.length==1)
         {
             return 0;
         }
-      
-            if(nums[0]>nums[1])
-            {
-                return 0;
-            }
-           
-        for(int i=1;i<nums.length-1;i++)
+        while(left<right)
         {
-        
-            if(nums[i]>nums[i-1] && nums[i]<nums[i+1])
+            int mid=(left+right)/2;
+            if(nums[mid]>nums[mid+1])
             {
-                continue;
+                right=mid;
             }
             else
             {
-                return i;
+                left=mid+1;
             }
         }
-        return nums.length-1;
+        return left;
     }
 }
