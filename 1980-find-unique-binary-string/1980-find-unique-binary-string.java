8593bf69df2626b3d9b7602815ac11ca72becfa3
Soumen Mondal <117503345+s479mondal@users.noreply.github.com>
2026-03-08 16:05:14 +0530

    Time: 4 ms (19.45%), Space: 43.7 MB (20.75%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String findDifferentBinaryString(String[] nums) {
+        String res="";
+        for(int i=0;i<nums.length;i++)
+        {
+            res+=nums[i].charAt(i)=='0'?'1':'0';
+        }
+        return res;
+    }
+}
