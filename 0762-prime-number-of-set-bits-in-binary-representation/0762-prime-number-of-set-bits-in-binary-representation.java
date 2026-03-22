093bee9975e26b5617f26eb0cafec7499eb37e36
Soumen Mondal <117503345+s479mondal@users.noreply.github.com>
2026-03-23 00:16:06 +0530

    Time: 7 ms (40.85%), Space: 42.6 MB (17.3%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.java
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.java
@@ -1,14 +1,8 @@
 class Solution {
     public int countPrimeSetBits(int left, int right) {
         HashSet<Integer> set=new HashSet<>();
-        set.add(2);
-        set.add(3);
-         set.add(5);
-                 set.add(7);
-         set.add(11);
-                 set.add(13);
-        set.add(17);
-        set.add(19);
+
+        set.add(2); set.add(3); set.add(5); set.add(7); set.add(11); set.add(13); set.add(17); set.add(19);
         int c=0;
         for(int i=left;i<=right;i++)
         {
@@ -18,5 +12,6 @@ class Solution {
             }
         }
         return c;
+
     }
 }
