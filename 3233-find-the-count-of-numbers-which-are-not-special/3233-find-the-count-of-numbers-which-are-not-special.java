2e37b0e51beb7c2b2affa0786443b11b7196e289
Soumen Mondal <117503345+s479mondal@users.noreply.github.com>
2026-03-23 00:08:14 +0530

    Time: 10 ms (95.68%), Space: 46.2 MB (14.59%) - LeetHub

--- 3233-find-the-count-of-numbers-which-are-not-special/3233-find-the-count-of-numbers-which-are-not-special.java
+++ 3233-find-the-count-of-numbers-which-are-not-special/3233-find-the-count-of-numbers-which-are-not-special.java
@@ -1,37 +1,34 @@
 class Solution {
-  
     public int nonSpecialCount(int l, int r) {
-          int length=r-l+1;
-
-          int s=(int) Math.ceil(Math.sqrt(l));
-          int e=(int) Math.floor(Math.sqrt(r));
-        
-          boolean b[]=new boolean[e+1];
-          b[0]=b[1]=false;
-          for(int i=2;i<=e;i++)
-          {
-                b[i]=true;
-          }
-          for(int i=2;i*i<=e;i++)
-          {
+        int length=r-l+1;
+        int start=(int)Math.ceil(Math.sqrt(l));
+        int end=(int)Math.floor(Math.sqrt(r));
+        int total=end-start+1;
+        boolean b[]=new boolean[end+1];
+        for(int i=2;i<=end;i++)
+        {
+            b[i]=true;
+        }
+        b[0]=false;
+        b[1]=false;
+        for(int i=2;i*i<=end;i++)
+        {
             if(b[i])
             {
-                for(int j=i+i;j<=e;j+=i)
+                for(int j=i*i;j<=end;j+=i)
                 {
                     b[j]=false;
                 }
             }
-          }
-          int c=0;
-          for(int i=s;i<=e;i++)
-          {
+        }
+        int c=0;
+        for(int i=start;i<=end;i++)
+        {
             if(b[i])
             {
                 c++;
             }
-          }
-          return length-c;
-
-
+        }
+        return length-c;
     }
 }
