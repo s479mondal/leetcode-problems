9d28fcbe456c6f2156bcaab841ca2fe52258a258
Soumen Mondal <117503345+s479mondal@users.noreply.github.com>
2026-05-03 00:10:40 +0530

    Time: 0 ms (100%), Space: 42.2 MB (35.37%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n<2 && n>0)
+        {
+            return 1;
+        }
+        int a=1;
+        int b=1;
+        int c=0;
+        for(int i=2;i<=n;i++)
+        {
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        return c;
+    }
+}
