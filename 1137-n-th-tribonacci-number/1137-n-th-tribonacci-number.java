257646cfe9b39a20bb52fc99a99363790e03506e
Soumen Mondal <117503345+s479mondal@users.noreply.github.com>
2026-05-11 01:20:19 +0530

    Time: 0 ms (100%), Space: 42.3 MB (15.36%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int tribonacci(int n) {
+        int a=0;
+        int b=1;
+        int c=1;
+        int d=a+b+c;
+        if(n==0) 
+        {
+            return a;
+        }
+        if(n==1) 
+        {
+            return b;
+        }
+        if(n==2)
+        {
+            return c;
+        }
+        for(int i=3;i<=n;i++)
+        {
+            d=a+b+c;
+            a=b;
+            b=c;
+            c=d;
+        }
+        return d;
+    }
+}
